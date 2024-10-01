bddd7e659b6bf2a6f64b991a64c51f9d8819b8e0
Diana Alcala <diana.alcalasandoval@gmail.com>
2024-09-30 21:46:28 -0600

    Integracion con backend

--- backend/MegaReports/Controllers/Ordenes_trabajo.cs
+++ backend/MegaReports/Controllers/Ordenes_trabajo.cs
@@ -49,9 +49,9 @@ namespace MegaReports.Controllers
                             JOIN Cuadrillas C ON C.Id = OT.CuadrillaId 
                             JOIN Suscriptores S ON S.Id = OT.SuscriptorId 
                             WHERE TE.Id = @Id
-                                AND OT.Estatus = 'Finalizado'
-                                AND OT.Tiempo_finalizado >= DATEADD(DAY, 1 - DATEPART(WEEKDAY, GETDATE()), CAST(GETDATE() AS DATE))
-                                AND OT.Tiempo_finalizado <  DATEADD(DAY, 8 - DATEPART(WEEKDAY, GETDATE()), CAST(GETDATE() AS DATE));";
+                                AND (OT.Tiempo_finalizado IS NULL
+                                    OR (OT.Tiempo_finalizado >= DATEADD(DAY, 1 - DATEPART(WEEKDAY, GETDATE()), CAST(GETDATE() AS DATE))
+                                        AND OT.Tiempo_finalizado < DATEADD(DAY, 8 - DATEPART(WEEKDAY, GETDATE()), CAST(GETDATE() AS DATE))));";
             var parameters = new[]
             {
                 new SqlParameter("@Id", id)
